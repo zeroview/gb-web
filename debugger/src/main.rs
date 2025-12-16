e3ff6ac39d2415182095658fdefc72e4fae66319
zeroview <zeroviewzero@gmail.com>
2025-12-16 22:36:41 +0200

    Bumped version to 1.0.0

--- debugger/src/main.rs
+++ debugger/src/main.rs
@@ -1,7 +1,14 @@
-use gb_web_core::CPU;
 use dotenv::dotenv;
+use gb_web_core::CPU;
 use std::{env, error, fs};
 
+/// This module simply loads a ROM from file path and runs it on the CPU
+/// It's ran locally instead of on WASM, so it can be used for debugging the emulator core
+///
+/// The configuration happens through a .env file in the working directory
+/// that has the following variables:
+/// EXECUTION_TIME: determines how many milliseconds the core is ran for at a time
+/// ROM_PATH: the local path to a ROM file
 pub fn main() -> Result<(), Box<dyn error::Error + 'static>> {
     dotenv().expect("No .env file found in working directory");
 
